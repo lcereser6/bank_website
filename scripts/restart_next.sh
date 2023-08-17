7bd8d854929474d3221d6c11a030bbefcab7a985
Shan <lorenzo.cereser@studenti.unitn.it>
2023-08-17 21:59:35 +0200

    Mod restart

--- scripts/restart_next.sh
+++ scripts/restart_next.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 cd /home/ubuntu/bank_website
-pm2 restart bank_website 
+#if it is not running, start it
+if ! pm2 list | grep -q 'bank_website'; then
+  pm2 start npm --name "bank_website" -- start
+#otherwise, restart it
+else
+  pm2 restart bank_website
+fi
