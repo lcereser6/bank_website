b6052daccc185b5640345953bb5855df196321b9
Shan <lorenzo.cereser@studenti.unitn.it>
2023-08-17 22:01:06 +0200

    Fixed string typo

--- scripts/restart_next.sh
+++ scripts/restart_next.sh
@@ -2,7 +2,7 @@
 cd /home/ubuntu/bank_website
 #if it is not running, start it
 if ! pm2 list | grep -q 'bank_website'; then
-  pm2 start npm --name "bank_website" -- start
+  pm2 start yarn --name bank_website -- start -p 3000
 #otherwise, restart it
 else
   pm2 restart bank_website
