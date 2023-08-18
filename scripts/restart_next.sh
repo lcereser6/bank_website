d997dab112852d9a528a0bc5373e832984150524
Shan <lorenzo.cereser@studenti.unitn.it>
2023-08-18 12:12:43 +0200

    Test update app runner

--- scripts/restart_next.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /home/ubuntu/bank_website
-#if it is not running, start it
-if ! pm2 list | grep -q 'bank_website'; then
-  pm2 start yarn --name bank_website -- start -p 3000
-#otherwise, restart it
-else
-  pm2 restart bank_website
-fi
